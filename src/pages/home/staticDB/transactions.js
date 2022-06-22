6ac6036c331acde2e318137e2d6656328a0bd136
KaGiraldo <104786464+KaGiraldo@users.noreply.github.com>
2022-06-22 16:54:04 -0500

    Update transactions.js

--- src/pages/home/staticDB/transactions.js
+++ src/pages/home/staticDB/transactions.js
@@ -2,8 +2,8 @@ import React from "react";
 import Card from '../portfoliodoc/transaction__card';
 
 function transactionList(){
-  
-    const cardsorig = [
+
+    const cards = [
         {
             cardTitle:"$1,000,000.00",
             cardInfo:"Payment of Installment",
@@ -12,20 +12,16 @@ function transactionList(){
         {
             cardTitle:"$1,000,000.00",
             cardInfo:"Payment of Installment",
-            date:"2022-06-10",
+            date:"2022-06-07",
         },
         {
             cardTitle:"$1,000,0132320.00",
             cardInfo:"Payment of Installment",
-            date:"2022-06-20",
+            date:"2022-06-07",
         },  {
             cardTitle:"$1,000,000.00",
             cardInfo:"Payment of Installment",
-            date:"2021-07-07",
-        },  {
-            cardTitle:"$1,000,000.00",
-            cardInfo:"Payment of Installment",
-            date:"2021-03-07",
+            date:"2021-06-07",
         },  {
             cardTitle:"$1,000,000.00",
             cardInfo:"Payment of Installment",
@@ -33,80 +29,19 @@ function transactionList(){
         },  {
             cardTitle:"$1,000,000.00",
             cardInfo:"Payment of Installment",
-            date:"2012-03-20",
+            date:"2021-03-07",
+        },  {
+            cardTitle:"$1,000,000.00",
+            cardInfo:"Payment of Installment",
+            date:"2012-03-07",
         },
     ]
 
-    var cards=cardsorig;
-
-    /* var filtercardsdate2 = cardsorig.filter(arrayfilter);
-    function arrayfilter(element){
-        return element.date=="2012-03-20";
-    }
-    console.log(filtercardsdate2); */
-
-    const hola ="qwnejwh";
-    var variable="Hola que hace";
-
-    function filterDate(){
-        const startDate = document.getElementById('date1');
-        const endDate = document.getElementById('date2');
-
-        const dates = []
-
-        for (let i = 0; i < cards.length; i++) {
-            dates[i]=cards[i].date;
-        }
-
-        const filteredDate = dates.filter(dateFilter);
-        function dateFilter(date){
-            return date >= startDate.value && date <= endDate.value;
-        }
-
-        var filtercardsdate=[];
-        for (let j = 0; j < filteredDate.length; j++) {
-            /* console.log(filteredDate[j]); */
-            const datesearch=filteredDate[j];
-
-            cardsorig.forEach(element => {
-                if (element.date==datesearch) {
-                    filtercardsdate.push(element);
-                }
-            });
-        }
-
-        cards=filtercardsdate;
-
-        for (let m = 0; m < cards.length; m++) {
-            <Card  cardTitle={card.cardTitle} cardInfo={card.cardInfo} date={card.date}/>            
-        }
-
-    }
 
 
-    console.log(cards);
-    const cardList = cards.map( card =>(
-
-    <Card  cardTitle={card.cardTitle} cardInfo={card.cardInfo} date={card.date}/>
-    
-    ))
-
+    const cardList = cards.map( card =>(<Card  cardTitle={card.cardTitle} cardInfo={card.cardInfo} date={card.date}/>))
     return (
      <div className="container-fluid">
-        <div className="container-fluid p-3 d-flex justify-content-around">
-               <div className="row w-50 p-2">
-                    <label className="text-primary">Initial Date</label>
-                    <input type="date" className=" border border-primary text-black border-0 border-bottom" id="date1"></input>
-                    
-
-               </div>
-               <div className="row w-50 p-2">
-                    <label className="text-primary">Final Date</label>
-                    <input type="date" className=" border border-primary text-black border-0 border-bottom" id="date2" onChange={filterDate}></input>
-               </div>
-
-
-        </div>
            {cardList}
      </div>
     )
